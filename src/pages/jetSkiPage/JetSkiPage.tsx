a6b64ac91a894cf6b7c9c8082945988b656bee92
Vadim <snooperv@gmail.com>
2023-03-24 04:01:31 +0500

    Плавная прокрутка к якорю

--- src/pages/jetSkiPage/JetSkiPage.tsx
+++ src/pages/jetSkiPage/JetSkiPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { MutableRefObject, useEffect, useRef, useState } from "react";
 import styles from "./jetSki.module.scss";
 import Promo from "../../components/jetSki/promo/Promo";
 import Banner from "../../components/jetSki/banner/Banner";
@@ -18,6 +18,7 @@ const JetSkiPage = () => {
   const PageNumber = Number(searchParams.get("PageNumber")) || 1;
   const [cards, setCards] = useState<cardProps[]>([]);
   const [pageCount, setPageCount] = useState<number>(1);
+  const h2ref = useRef() as MutableRefObject<HTMLDivElement>;
 
   useEffect(() => {
     if (String(searchParams).length === 0)
@@ -35,6 +36,7 @@ const JetSkiPage = () => {
   }, [searchParams]);
 
   const changePage = (e: React.ChangeEvent<unknown>, page: number) => {
+    h2ref.current.scrollIntoView();
     let query = [...searchParams];
     query[0][1] = String(page);
     setSearchParams(query);
@@ -49,7 +51,7 @@ const JetSkiPage = () => {
       </div>
       <SearchName />
       <PageTitle />
-      <div className={styles.mainContent}>
+      <div className={styles.mainContent} ref={h2ref}>
         <Filters />
         <div className={styles.cardsContent}>
           <div className={styles.cards}>
